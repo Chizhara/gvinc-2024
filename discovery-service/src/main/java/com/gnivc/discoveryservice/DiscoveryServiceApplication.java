e58f0707bcf927257d4782ec241f6650ea96059f
Chizhara <federico.chizhara@yandex.ru>
2024-05-23 00:42:56 +0300

    refactor: preparing discovery-service environment

--- discovery-service/src/main/java/com/gnivc/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/gnivc/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package gnivc.com.discoveryservice;
+package com.gnivc.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
 
 }
