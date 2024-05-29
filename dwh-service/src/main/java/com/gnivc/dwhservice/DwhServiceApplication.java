3fdb80bd6701703ff30e87b6e05bfee8e0e88ebf
Fedor Chizhik <federico.chizhara@yandex.ru>
2024-05-29 03:03:07 +0300

    Homework 1 (#1)
    * refactor: add services. add gateway

---
+++ dwh-service/src/main/java/com/gnivc/dwhservice/DwhServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gnivc.dwhservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwhServiceApplication.class, args);
+    }
+
+}
---
+++ dwh-service/src/test/java/com/gnivc/dwhservice/DwhServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnivc.dwhservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwhServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
